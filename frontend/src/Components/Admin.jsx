c78665e5e5b72969d8c8cf608fd17832d1912e89
Aryan <aryanrajput039@gmail.com>
2025-06-13 21:47:30 +0530

    admin page progress

--- frontend/src/Components/Admin.jsx
+++ frontend/src/Components/Admin.jsx
@@ -6,14 +6,15 @@ import '../styles/Admin.css'
 export const Employees = () => {
   return (
     <div className="employee-container">
-      <h3>New Applications</h3>
-      <table className="employee-table">
+      <h3>New Applications*</h3>
+
+      <table>
         <thead>
           <tr>
             <th>Serial No.</th>
             <th>Name</th>
             <th>Position</th>
-            <th>View</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
@@ -31,6 +32,46 @@ export const Employees = () => {
           </tr>
         </tbody>
       </table>
+
+      <h3 style={{marginTop:"3rem"}}>Employees</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>EmpId</th>
+            <th>Name</th>
+            <th>Designation</th>
+            <th>Department</th>
+            <th>Location</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>101</td>
+            <td>John Doe</td>
+            <td>Teacher</td>
+            <td>Education</td>
+            <td>New York</td>
+            <td><button>View Details</button></td>
+          </tr>
+          <tr>
+            <td>102</td>
+            <td>Jane Smith</td>
+            <td>Multi-Tasking Staff</td>
+            <td>Support</td>
+            <td>Los Angeles</td>
+            <td><button>View Details</button></td>
+          </tr>
+          <tr>
+            <td>103</td>
+            <td>Emily Johnson</td>
+            <td>Volunteer</td>
+            <td>Community Service</td>
+            <td>Chicago</td>
+            <td><button>View Details</button></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
@@ -43,7 +84,54 @@ export const Reports = () => {
 
 export const Blogs = () => {
   return (
-    <div>Blogs</div>
+    <div className="blogs-container">
+      <button>Add New</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Serial No.</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Published on</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Clean Foundation's Impact</td>
+            <td>John Doe</td>
+            <td>10th September 2024</td>
+            <td><button>Edit</button></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Community Clean-Up Day</td>
+            <td>Jane Smith</td>
+            <td>1st January 2025</td>
+            <td><button>Edit</button></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Volunteer Stories</td>
+            <td>Emily Johnson</td>
+            <td>3rd March 2025</td>
+            <td><button>Edit</button></td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Fundraising Success</td>
+            <td>Michael Brown</td>
+            <td>19 August 2099</td>
+            <td><button>Edit</button></td>
+          </tr>
+        </tbody>
+
+      </table>
+
+      
+    </div>
   )
 }
 
